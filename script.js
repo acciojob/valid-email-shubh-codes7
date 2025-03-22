539dfa8c6d80bd6c1c24652604391de09cdae480
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-22 09:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validEmail(str) {
-  //your JS code here.
+    let regex = [a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}
+	return regex.test(str)
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address."); 
 alert(validEmail(str));
